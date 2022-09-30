936218c1a5d74a0b6f20aa15fce7d09d8c865c2a
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-01 01:49:23 +0300

    Create Statistics.jsx

---
+++ src/components/Feedback/Statistics.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Statistics = ({ good, neutral, bad }) => 
+  <ul>
+    <li>Good: {good}</li>
+    <li>Neutral: {neutral}</li>
+    <li>Bad: {bad}</li>
+    <li>Total: </li>
+    <li>Positive feedback: % </li>
+  </ul>;
+;
+
+export default Statistics;
