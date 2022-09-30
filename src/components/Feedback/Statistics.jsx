f18210287c25baa37bccf59fa2ad2ebca2c8ccac
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-01 02:19:23 +0300

    Update Statistics.jsx

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Statistics = ({ good, neutral, bad }) => 
-  <ul>
-    <li>Good: {good}</li>
-    <li>Neutral: {neutral}</li>
-    <li>Bad: {bad}</li>
-    <li>Total: </li>
-    <li>Positive feedback: % </li>
-  </ul>;
-;
-
+const Statistics = ({ good, neutral, bad }) =>
+ 
+<ul>
+  <li>Good: {good}</li>
+  <li>Neutral: {neutral}</li>
+  <li>Bad: {bad}</li>
+  <li>Total: </li>
+  <li>Positive feedback: % </li>
+</ul>;
 export default Statistics;
