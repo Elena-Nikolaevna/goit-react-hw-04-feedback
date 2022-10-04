d754ae7000ca8e7cd9cc0c262945bc95c232385f
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-04 18:29:15 +0300

    Update Section.jsx

--- src/components/Feedback/Section.jsx
+++ src/components/Feedback/Section.jsx
@@ -1,4 +1,4 @@
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import css from './Feedback.module.css';
 const Section = ({ title, children }) => (
   <>
@@ -10,5 +10,6 @@ const Section = ({ title, children }) => (
 export default Section;
 
 Section.propTypes = {
-  title: propTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
