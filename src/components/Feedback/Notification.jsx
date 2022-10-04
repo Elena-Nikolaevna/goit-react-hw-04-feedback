03a2c78607981be5f321cbdc594d7742e7b96a5a
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-04 18:29:13 +0300

    Update Notification.jsx

--- src/components/Feedback/Notification.jsx
+++ src/components/Feedback/Notification.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
 import css from './Feedback.module.css';
 const Notification = ({ message }) => {
 	return <h2 className={css.title}>{message}</h2>;
 };
 
 export default Notification;
+
+Notification.propTypes = {
+	message: PropTypes.string.isRequired,
+	
+  };
