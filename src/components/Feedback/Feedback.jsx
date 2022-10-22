3e6da8f4d806baaca070da98fd72ea8c300f6213
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-22 16:56:13 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,73 @@
-import React from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import ButtonsFeedback from './ButtonsFeedback';
 import Notification from './Notification';
 import Statistics from './Statistics';
 import css from './Feedback.module.css';
+import PropTypes from 'prop-types';
 
+export default function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-class Feedback extends React.Component {
+  const feedbackButton = ['Good', 'Neutral', 'Bad'];
+
+  const countFeedback = evt => {
+    const name = evt.currentTarget.innerText;
+    switch (name) {
+        case 'Good':
+            setGood(good + 1);
+            break;
+        case 'Neutral':
+            setNeutral(neutral + 1);
+            break;
+        case 'Bad':
+            setBad(bad + 1);
+            break;
+        default:
+        return;
+    }
+};
+const countTotalFeedback = () => {
+  const total = good + neutral + bad;
+  return total;
+};
+const countPositiveFeedbackPercentage = () => {
+  const positiveFeedback = Math.floor((good * 100) / countTotalFeedback());
+  return positiveFeedback;
+};
+
+  return (
+    <div className={css.feedback}>
+      <Section title="Please leave feedback!">
+        <ButtonsFeedback
+          options={feedbackButton}
+          onCountFeedback={countFeedback}
+        />
+      </Section>
+
+      {countTotalFeedback() === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            options={{ good, neutral, bad }}
+            onCountTotalFeedback={countTotalFeedback()}
+            onCountPositiveFeedbackPercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      )}
+    </div>
+  );
+}
+
+Feedback.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,    
+};
+/* class Feedback extends React.Component {
   state = {
     good: 0,
     neutral: 0,
@@ -15,24 +76,7 @@ class Feedback extends React.Component {
   countFeedback = button => {
     this.setState({ [button]: this.state[button] + 1 });
   };
-  /* addGood = () => {
-    this.setState(prevState => ({
-      good: prevState.good + 1,
-    }));
-  };
-
-  addNeutral = () => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral + 1,
-    }));
-  };
-
-  addBad = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1,
-    }));
-  };
- */
+  
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
@@ -73,3 +117,4 @@ class Feedback extends React.Component {
   }
 }
 export default Feedback;
+ */
