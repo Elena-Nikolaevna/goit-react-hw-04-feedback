b71334ac8f61b271c1c1513830eb2ee902c22ce3
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-23 22:41:41 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,39 +4,39 @@ import ButtonsFeedback from './ButtonsFeedback';
 import Notification from './Notification';
 import Statistics from './Statistics';
 import css from './Feedback.module.css';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
 export default function Feedback() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const feedbackButton = ['Good', 'Neutral', 'Bad'];
+  const feedbackButton = Object.keys({ good, neutral, bad });
 
   const countFeedback = evt => {
     const name = evt.currentTarget.innerText;
     switch (name) {
-        case 'Good':
-            setGood(good + 1);
-            break;
-        case 'Neutral':
-            setNeutral(neutral + 1);
-            break;
-        case 'Bad':
-            setBad(bad + 1);
-            break;
-        default:
+      case 'Good':
+        setGood(good + 1);
+        break;
+      case 'Neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'Bad':
+        setBad(bad + 1);
+        break;
+      default:
         return;
     }
-};
-const countTotalFeedback = () => {
-  const total = good + neutral + bad;
-  return total;
-};
-const countPositiveFeedbackPercentage = () => {
-  const positiveFeedback = Math.floor((good * 100) / countTotalFeedback());
-  return positiveFeedback;
-};
+  };
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
+    return total;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const positiveFeedback = Math.floor((good * 100) / countTotalFeedback());
+    return positiveFeedback;
+  };
 
   return (
     <div className={css.feedback}>
@@ -62,11 +62,7 @@ const countPositiveFeedbackPercentage = () => {
   );
 }
 
-Feedback.propTypes = {
-  good: PropTypes.number,
-  neutral: PropTypes.number,
-  bad: PropTypes.number,    
-};
+
 /* class Feedback extends React.Component {
   state = {
     good: 0,
